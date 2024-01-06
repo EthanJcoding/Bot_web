56e95823dca9fd457f04adb8a5202073317d5ff7
ethanJcoding <ethan.95j@gmail.com>
2024-01-06 22:23:21 +0900

    Feat: ACS input dialog created

--- src/components/GameDashboard/elements/AcsInputDialog.tsx
+++ src/components/GameDashboard/elements/AcsInputDialog.tsx
@@ -13,7 +13,29 @@ import {
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+import { useState } from 'react'
+import TierListPopover from './TierList/TierListPopover'
+import TierToggle from './TierList/TierToggle'
+
 const AcsInputDialog = () => {
+  const [selectedTier, setSelectedTier] = useState('')
+  const [selectedTierSegment, setSelectedTierSegment] = useState('')
+  const [open, setOpen] = useState<boolean>(false)
+  const [acs, setAcs] = useState('')
+
+  const handleClickList = (tier: string) => {
+    setSelectedTier(tier)
+    setOpen(false)
+  }
+
+  const handleInputAcs = (acs: string) => {
+    setAcs(acs)
+  }
+
+  const handleSubmit = () => {
+    console.log(selectedTier, selectedTierSegment, acs)
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -21,7 +43,7 @@ const AcsInputDialog = () => {
           실력 지표 입력하기
         </button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] ">
         <DialogHeader>
           <DialogTitle>실력 지표 입력하기</DialogTitle>
           <DialogDescription>
@@ -33,17 +55,37 @@ const AcsInputDialog = () => {
             <Label htmlFor="tier" className="text-right">
               Tier
             </Label>
-            <Input id="tier" value="Pedro Duarte" className="col-span-3" />
+            <div className="col-span-2">
+              <TierListPopover
+                open={open}
+                onOpenChange={(open) => setOpen(open)}
+                setSelectedTier={(tier) => handleClickList(tier)}
+                selectedTier={selectedTier}
+              />
+            </div>
+            <div className="w-full flex justify-between space-x-2">
+              <TierToggle
+                setSelectedTierSegment={(segment) =>
+                  setSelectedTierSegment(segment)
+                }
+              />
+            </div>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="acs" className="text-right">
               ACS
             </Label>
-            <Input id="acs" value="@peduarte" className="col-span-3" />
+            <Input
+              id="acs"
+              type="number"
+              value={acs}
+              onChange={(e) => handleInputAcs(e.target.value)}
+              className="col-span-3 outline-none"
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button onClick={() => handleSubmit()}>Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
